9e3794a5698cfc0f5bc4c0899f3db9757babcacd
Yermek <yermek20@gmail.com>
2023-03-17 00:07:58 +0600

    Task 5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double divider = 5;
+        System.out.println(toBeDivided / divider);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+
+        System.out.println(minutes * 60);
     }
 }
